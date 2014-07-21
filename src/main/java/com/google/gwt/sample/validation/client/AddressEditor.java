54adef47b575d681bcd1593d6d2ee2b5b33efda8
Tripathi <attripat@406c8f1f34da.ant.amazon.com>
2014-07-20 17:05:41 -0700

    Editor decorator with red-color error msg on right

--- src/main/java/com/google/gwt/sample/validation/client/AddressEditor.java
+++ src/main/java/com/google/gwt/sample/validation/client/AddressEditor.java
@@ -14,6 +14,7 @@ import com.google.gwt.editor.client.Editor;
 import com.google.gwt.editor.client.SimpleBeanEditorDriver;
 import com.google.gwt.editor.ui.client.ValueBoxEditorDecorator;
 import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.sample.validation.client.widget.BetterValueBoxEditorDecorator;
 import com.google.gwt.sample.validation.shared.Address;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
@@ -44,9 +45,9 @@ public class AddressEditor extends Composite implements Editor<Address> {
     }
 
     @UiField
-    ValueBoxEditorDecorator<String> street;
+    BetterValueBoxEditorDecorator<String> street;
     @UiField
-    ValueBoxEditorDecorator<String> zip;
+    BetterValueBoxEditorDecorator<String> zip;
     @UiField
     Button save;
     @UiField
